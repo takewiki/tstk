6651e9d1d7dbd08cfb089625168eb924eee9579b
takewiki <hulilei@takewiki.com.cn>
2022-05-07 17:08:00 +0800

    v3.2

---
+++ R/pkg.R
@@ -0,0 +1,37 @@
+#' 导入包
+#'
+#' @param conn 连接
+#' @param FappId 软件程序
+#'
+#' @return
+#'
+#' @examples
+#' importPkgs()
+importPkgs <- function(conn=tsda::conn_rds('rdbe'),FappId ='system') {
+sql <- paste0(" select  FPkgName from t_md_pkgs
+ where FappId ='",FappId,"' and Fshow =1")  
+data = tsda::sql_select(conn,sql)
+ncount = nrow(data)
+if(ncount >0){
+  pkgs = data$FPkgName
+  res <- pacman::p_load(char = pkgs)
+}
+
+  
+}
+
+#' 加载包
+#'
+#' @param app_id 
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#'import() 
+import <- function(app_id) {
+  importPkgs()
+  importPkgs(FappId = app_id)
+  
+  
+}
