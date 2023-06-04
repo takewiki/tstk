8b62b979ecf5e257c957c1f653491737846a8e09
takewiki <hulilei@takewiki.com.cn>
2023-06-04 17:06:58 +0800

    v3.3

--- R/updateSciptFiles.R
+++ R/updateSciptFiles.R
@@ -6,5 +6,14 @@
 #'
 #' @examples updateScriptFiles();
 updateScriptFiles <- function(){
+  #删除演示数据
+  if(file.exists('R/hello.R')){
+    file.remove('R/hello.R')
+  }
+  #删除namespace重新生成
+  if(file.exists('NAMESPACE')){
+    file.remove('NAMESPACE')
+  }
+  #更新文件
   devtools::document()
 }
